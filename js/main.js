e7d1f03bdf8fa8648b3a0c87bfa27d380f52aa58
Jaloliddin-Zokirov <jaloliddinzokirov1908@gmail.com>
2022-06-05 02:58:24 +0500

    Add editor hamburger menu

--- js/main.js
+++ js/main.js
@@ -4,3 +4,21 @@ var elSpan = document.querySelector('.site-body')
 elBody.addEventListener('click', function () {
   elSpan.classList.toggle('site-body--white')
 })
+
+
+
+
+
+var elIcon = document.querySelector('.header__icon')
+
+elIcon.addEventListener('click', function () {
+  elIcon.classList.toggle('open')
+})
+
+
+
+var elRight = document.querySelector('.header__right')
+
+elSpan.addEventListener('click', function () {
+  elRight.classList.toggle('header__right--on')
+})
