b7b8de49e6e3a7f7be8ba08b0db75dd33ac6687b
Jaloliddin-Zokirov <jaloliddinzokirov1908@gmail.com>
2022-06-06 03:37:29 +0500

    Add footer teg

--- js/main.js
+++ js/main.js
@@ -22,3 +22,12 @@ var elRight = document.querySelector('.header__right')
 elIcon.addEventListener('click', function () {
   elRight.classList.toggle('header__right--on')
 })
+
+
+
+// var elInp = document.querySelector('.price__choice')
+// var elBtn = document.querySelector('.price__order')
+
+// elInp.addEventListener('click', function () {
+//   elBtn.classList.toggle('on')
+// })
